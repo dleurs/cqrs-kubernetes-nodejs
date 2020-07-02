92675feb77bebffe6b281194f82c2bb5deda23e7
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-02 14:05:17 +0200

    Working using redirect

--- EntryPoint/src/main.ts
+++ EntryPoint/src/main.ts
@@ -1,16 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import querystring from 'querystring';
 
 const app: express.Application = express();
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res, _) => // _ = next
+const responseUrl: string = process.env.RESPONSEURL || "http://localhost:8081"//"http://response.default.svc.cluster.local";
+
+app.get('/', (_, res, __) => // _ = next
 {
-  console.log("URL : ", req.url, "\nMETHOD : ", req.method, "\nHEADERS : ", req.headers);
-  res.send('<h1>Hello World!</h1>');
+  const data = querystring.stringify({
+    "a": 1,
+    "b": 2,
+    "msg": "Hello World"
+  });
+  res.redirect(responseUrl + '/?' + data);
 });
 
-//const hostname: string = process.env.HOST_ADDR || "0.0.0.0";
 const port: string = process.env.PORT || "8080";
 const nodeEnv: string = process.env.NODE_ENV || "development";
 app.listen(parseInt(port), function () 
