46c093974a5e80d97c57c5f3cb32d131ec32d3a3
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-03 14:32:32 +0200

    request response format implemented

--- EntryPoint/src/main.ts
+++ EntryPoint/src/main.ts
@@ -1,25 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import querystring from 'querystring';
+import axios from 'axios';
 
 const app: express.Application = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const responseUrl: string = process.env.RESPONSEURL || "http://localhost:8081"
 
-app.get('/', (_, res, __) => // _ = next
+app.get('/', async (_, res, ___) => // _ ,__, ___ = req, res, next
 {
+  console.log(`EntryPoint "/" request received`);
   const data = querystring.stringify({
-    "a": 1,
-    "b": 2,
-    "msg": "Hello World"
+    "msg": "John"
   });
-  res.redirect(responseUrl + '/?' + data);
+  try
+  {
+    //const response = await axios.get(responseUrl + '/?' + data); // req.query == "msg": "John"
+    const response = await axios.post(responseUrl, data); // req.body == "msg": "John"
+    return res.send(response.data);
+  } catch (exception)
+  {
+    process.stderr.write(`ERROR received from ${responseUrl}: ${exception}\n`);
+  }
+  return null;
+
 });
 
 const port: string = process.env.PORT || "8080";
 const nodeEnv: string = process.env.NODE_ENV || "development";
 app.listen(parseInt(port), function () 
 {
-  console.log(`Server running at http://localhost:${port}/ in ${nodeEnv}`);
+  console.log(`EntryPoint running at http://localhost:${port}/ in ${nodeEnv}`);
+  console.log(`RESPONSEURL : ${responseUrl}`)
 });
--- Response/src/main.ts
+++ Response/src/main.ts
@@ -7,7 +7,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.all('/', (req, res, __) => // _ = next
 {
   console.log(req);
-  console.log(res);
   res.send("<h1>"+ req.query.msg + "</h1>")
 });
 
@@ -15,5 +14,5 @@ const port: string = process.env.PORT || "8081";
 const nodeEnv: string = process.env.NODE_ENV || "development";
 app.listen(parseInt(port), function () 
 {
-  console.log(`Server running at http://localhost:${port}/ in ${nodeEnv}`);
+  console.log(`Response running at http://localhost:${port}/ in ${nodeEnv}`);
 });
