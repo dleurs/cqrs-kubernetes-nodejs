d6e040bb08da83ecb04baa9b2f53cb0f2d7e147b
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-03 15:45:06 +0200

    Should I implement it like this ?

--- ReadAndWriteDB/src/main.ts
+++ ReadAndWriteDB/src/main.ts
@@ -4,14 +4,17 @@ import bodyParser from 'body-parser';
 const app: express.Application = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res, _) => // _ = next
+let wormDb: Array<String> = Array<String>();
+
+app.post('/', (req, res, _) => // _ = next
 {
-  console.log("URL : ", req.url, "\nMETHOD : ", req.method, "\nHEADERS : ", req.headers);
-  res.send('<h1>Hello World!</h1>');
+  console.log("ReadAndWriteDB : Request received");
+  
+  res.send(200);
 });
 
 //const hostname: string = process.env.HOST_ADDR || "0.0.0.0";
-const port: string = process.env.PORT || "8080";
+const port: string = process.env.PORT || "8083";
 const nodeEnv: string = process.env.NODE_ENV || "development";
 app.listen(parseInt(port), function () 
 {
