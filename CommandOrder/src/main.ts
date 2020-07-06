8377d29be56907c222aa294b671e0e2351cec504
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-06 18:08:45 +0200

    Link between dispatcher and CommandOrder successful locally

---
+++ CommandOrder/src/main.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app: express.Application = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const orderDbUrl: string = process.env.COMMANDORDERURL || "http://localhost:8082"
+const processDataUrl: string = process.env.QUERYORDERBURL || "http://localhost:8083"
+
+app.post('/', (_, res, __) => // _ = next
+{
+  console.log(`Request received`);
+  //console.log(req);
+  res.status(200).send();
+});
+
+const port: string = process.env.PORT || "8081";
+const nodeEnv: string = process.env.NODE_ENV || "development";
+
+app.listen(parseInt(port), function () 
+{
+  console.log(`CommandOrder running at http://localhost:${port}/ in ${nodeEnv}`);
+  console.log(`orderDbUrl : ${orderDbUrl}`);
+  console.log(`processDataUrl : ${processDataUrl}`);
+});
--- Dispatcher/src/main.ts
+++ Dispatcher/src/main.ts
@@ -1,64 +1,39 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import querystring from 'querystring';
+//import querystring from 'querystring';
 import axios from 'axios';
 
 const app: express.Application = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
-const queryUrl: string = process.env.QUERYURL || "http://localhost:8081"
-const eventLogDbUrl: string = process.env.EVENTLOGDBURL || "http://localhost:8084"
+const commandOrderUrl: string = process.env.COMMANDORDERURL || "http://localhost:8081"
+const queryOrderUrl: string = process.env.QUERYORDERBURL || "http://localhost:8084"
+
+app.post('/order', async (req, res, _) => {
+  //console.log(req);
+  try
+  {
+    await axios.post(commandOrderUrl, req.body); // req.body == "msg": "John"
+    return res.status(200).send("Action received and being treated");
+  } catch (exception)
+  {
+    process.stderr.write(`ERROR in Dispatcher for url ${commandOrderUrl}: ${exception}\n`);
+    return res.status(500).send(`ERROR in Dispatcher for url ${commandOrderUrl}: ${exception}\n`);
+  }
+});
 
 app.get('/', async (_, res, ___) => // _ ,__, ___ = req, res, next
 {
-  console.log(`serviceInterface GET "/" request received`);
-  const data = querystring.stringify({
-    "msg": "John"
-  });
-  try
-  {
-    //const query = await axios.get(queryUrl + '/?' + data); // req.query == "msg": "John"
-    const query = await axios.post(queryUrl, data); // req.body == "msg": "John"
-    return res.send(query.data);
-  } catch (exception)
-  {
-    process.stderr.write(`ERROR received from ${queryUrl}: ${exception}\n`);
-  }
-  return null;
-
-});
-
-app.get('/user', (_, res, ___) => // _ ,__, ___ = req, res, next
-{
-  console.log(`serviceInterface GET "/user" request received`);
-  const addUserForm: string = `<h1>Add a user</h1> <form action="/user" method="POST"><input type="text" name="title"><button type="submit">Add User</button></form>`;
-  return res.send(addUserForm);
-});
-
-app.post('/user', async (req, res, _) => // _ ,__, ___ = req, res, next
-{
-  console.log(`serviceInterface POST "/user" request received`);
-  const data = querystring.stringify({
-    "type": "create",
-    "object": req.body.title
-  });
-  try
-  {
-    const query = await axios.post(eventLogDbUrl, data);
-    console.log("query" + query.toString());
-    return res.send("<h1>Command executed</h1>");
-  } catch (exception)
-  {
-    process.stderr.write(`ERROR received from ${eventLogDbUrl}: ${exception}\n`);
-  }
-  return null;
+  res.write(`<h1>Welcome into a simple CQRS example</h1>`);
+  res.write(`<h2>Hosted on Kubernetes and coded with NodeJS / TypeScript</h2>`);
+  res.send();
 });
 
 const port: string = process.env.PORT || "8080";
 const nodeEnv: string = process.env.NODE_ENV || "development";
 app.listen(parseInt(port), function () 
 {
-  console.log(`serviceInterface running at http://localhost:${port}/ in ${nodeEnv}`);
-  console.log(`queryURL : ${queryUrl}`);
-  console.log(`eventLogDbUrl : ${eventLogDbUrl}`);
+  console.log(`Dispatcher running at http://localhost:${port}/ in ${nodeEnv}`);
+  console.log(`commandOrderUrl : ${commandOrderUrl}`);
+  console.log(`queryOrderUrl : ${queryOrderUrl}`);
 });
