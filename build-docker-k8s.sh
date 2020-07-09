40112509223b5ad40ff8fad4b7c20f092000d9c3
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-09 16:19:04 +0200

    build docker k8s for all pods

--- build-docker-k8s.sh
+++ build-docker-k8s.sh
@@ -16,6 +16,12 @@ if [ "$#" -ne 2 ]; then
     echo "[Arguments missing] image tags and service name required, like ./build-docker-k8s.sh 1.0.0 dispatcher or ./build-docker-k8s.sh 1.0.0 all"
 elif [ $2 = "all" ]; then
     echo "Building all images with tag $1"
+    createImageAndK8s $1 "command-order"
+    createImageAndK8s $1 "dispatcher"
+    createImageAndK8s $1 "order-db"
+    createImageAndK8s $1 "process-data"
+    createImageAndK8s $1 "query-order"
+    createImageAndK8s $1 "stats-order-db"
 else
     echo "Building a single image"
     createImageAndK8s $1 $2
