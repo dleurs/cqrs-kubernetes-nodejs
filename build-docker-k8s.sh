0e239770ad5ea50315f6a5135aadd0b5f12bb291
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-13 10:36:18 +0200

    Adding script run k8s

--- build-docker-k8s.sh
+++ build-docker-k8s.sh
@@ -11,17 +11,28 @@ createImageAndK8s () {
     kubectl create -f $2.yaml
     kubectl replace -f $2.yaml
 }
+services=(
+    "command-order"
+    "dispatcher"
+    "order-db"
+    "process-data"
+    "query-order"
+    "stats-order-db"
+)
+
+if [ "$#" -eq 1 ] && [ $1 == "run-k8s" ]; then
+    for i in "${services[@]}"; do
+        kubectl create -f k8s/"$i".yaml
+    done
+fi
 
 if [ "$#" -ne 2 ]; then
-    echo "[Arguments missing] image tags and service name required, like ./build-docker-k8s.sh 1.0.0 dispatcher pseudo-docker or ./build-docker-k8s.sh 1.0.0 all pseudo-docker"
+    echo "[Arguments missing] image tags and service name required, like ./build-docker-k8s.sh 1.0.0 dispatcher pseudo-docker or ./build-docker-k8s.sh 1.0.0 all pseudo-docker. If you just want to create k8s services, ./build-docker-k8s.sh run-k8s"
 elif [ $2 = "all" ]; then
     echo "Building all images with tag $1 hosted with docker pseudo $3"
-    createImageAndK8s $1 "command-order" $3
-    createImageAndK8s $1 "dispatcher" $3
-    createImageAndK8s $1 "order-db" $3
-    createImageAndK8s $1 "process-data" $3
-    createImageAndK8s $1 "query-order" $3
-    createImageAndK8s $1 "stats-order-db" $3
+    for i in "${services[@]}"; do
+        createImageAndK8s $1 "$i" $3
+    done
 else
     echo "Building a single image with tag $1 hosted with docker pseudo $3"
     createImageAndK8s $1 $2 $3
