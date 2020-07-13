ff112e427881e9d1d2ee5cea270a49281f47ca7c
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-07-13 11:17:01 +0200

    Error with dispatcher

--- build-docker-k8s.sh
+++ build-docker-k8s.sh
@@ -1,33 +1,44 @@
 createImageAndK8s () {
-    echo "Building image, $2 $1"
+    echo "Building image $3/cqrs-$2:$1"
     cp -a utils $2/
     cd $2
-    docker build . -t dleurs/cqrs-$2:$1
+    docker build . -t $3/cqrs-$2:$1
     docker push $3/cqrs-$2:$1
     /bin/rm -rf utils
     cd ../k8s
     sed "s/IMAGETAG/$1/g" NOIMAGETAG/$2-NOIMAGETAG.yaml > $2.yaml #sed `s/NOIMAGETAB/$1/g` dispatcher.yaml 
     #kubectl delete -f $2.yaml
     kubectl create -f $2.yaml
+    cd ..
     kubectl replace -f $2.yaml
 }
 services=(
-    "command-order"
     "dispatcher"
+    "command-order"
     "order-db"
     "process-data"
     "query-order"
     "stats-order-db"
 )
 
+# kubectl create
 if [ "$#" -eq 1 ] && [ $1 == "run-k8s" ]; then
     for i in "${services[@]}"; do
         kubectl create -f k8s/"$i".yaml
     done
-fi
-
-if [ "$#" -ne 2 ]; then
-    echo "[Arguments missing] image tags and service name required, like ./build-docker-k8s.sh 1.0.0 dispatcher pseudo-docker or ./build-docker-k8s.sh 1.0.0 all pseudo-docker. If you just want to create k8s services, ./build-docker-k8s.sh run-k8s"
+# kubectl replace
+elif [ "$#" -eq 1 ] && [ $1 == "replace-k8s" ]; then
+    for i in "${services[@]}"; do
+        kubectl replace -f k8s/"$i".yaml
+    done
+# kubectl delete
+elif [ "$#" -eq 1 ] && [ $1 == "delete-k8s" ]; then
+    for i in "${services[@]}"; do
+        kubectl delete -f k8s/"$i".yaml
+    done
+# docker build and kubernetes create
+elif [ "$#" -ne 3 ]; then
+    echo "[Arguments missing] image tags and service name required, like ./build-docker-k8s.sh 1.0.0 dispatcher pseudo-docker or ./build-docker-k8s.sh 1.0.0 all pseudo-docker. If you just want to create k8s services, ./build-docker-k8s.sh (run-k8s | delete-k8s)"
 elif [ $2 = "all" ]; then
     echo "Building all images with tag $1 hosted with docker pseudo $3"
     for i in "${services[@]}"; do
